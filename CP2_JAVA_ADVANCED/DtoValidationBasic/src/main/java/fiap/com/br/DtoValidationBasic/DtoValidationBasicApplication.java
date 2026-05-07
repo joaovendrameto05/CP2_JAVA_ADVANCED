dea563ba4cfced131acb9e8d4cb80fc7faeeddce
joaovendrameto05 <joaovendrameto5mh@gmail.com>
2026-05-07 16:24:27 -0300

    Adicionando trabalha primário fornecido
    Adicionei o trabalho fornecido pelo professor.

---
+++ CP2_JAVA_ADVANCED/DtoValidationBasic/src/main/java/fiap/com/br/DtoValidationBasic/DtoValidationBasicApplication.java
@@ -0,0 +1,20 @@
+package fiap.com.br.DtoValidationBasic;
+
+/*--------------------------------------------------
+ *
+ * Programa: Exemplo de Java com Bean Validation
+ * Desenvolvido por: Prof. Dr. Marcel S. Wagner
+ *
+ *-------------------------------------------------*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoValidationBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoValidationBasicApplication.class, args);
+	}
+
+}
